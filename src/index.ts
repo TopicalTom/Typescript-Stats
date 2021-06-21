e303659aaadadb748ddfd1a93fd0fccaca63786f
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-20 23:25:42 -0400

    Extracted Match reader through inheritance to improve reusability

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const matchReader = new MatchReader('football.csv')
+matchReader.read();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
